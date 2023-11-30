11c61865a7b65802178e1ec5f6503bdff56ad18f
Tidalu <ul0271069@edu.uni.lodz.pl>
2023-11-30 09:48:28 +0100

    setting new properties and values : dot notation

--- class 3/main.js
+++ class 3/main.js
@@ -1,1 +1,34 @@
 // Your JavaScript code goes here
+
+
+var userOne = {
+    email: "ulugio23@gmail.com", 
+    name: "alif",
+    login(){
+        console.log(this.email, "has logged in")
+    }, 
+    logout(){
+        console.log(this.email, "has logged out")
+    }
+};
+
+var userTwo = {
+    email: "uewrwo23@gmail.com", 
+    name: "alrt",
+    login(){
+        console.log(this.email, "has logged in")
+    }, 
+    logout(){
+        console.log(this.email, "has logged out")
+    }
+};
+
+
+
+userOne.name = "Tidalu"
+
+
+var prop = "email"
+userOne[prop] ///it is possible 
+userOne.prop // this is not possible
+
