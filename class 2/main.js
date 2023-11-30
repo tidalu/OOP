46971aa4677fd15f8969b0f4617d1f15af0a92b3
Tidalu <ul0271069@edu.uni.lodz.pl>
2023-11-30 09:39:53 +0100

    creating object

--- class 2/main.js
+++ class 2/main.js
@@ -1,1 +1,15 @@
 // Your JavaScript code goes here
+
+
+var useOne = {
+    email: "ulugio23@gmail.com", 
+    name: "alif",
+    login(){
+        console.log(this.email, "has logged in")
+    }, 
+    logout(){
+        console.log(this.email, "has logged out")
+    }
+};
+
+console.log(useOne.name)
